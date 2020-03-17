9e2e37e7a13502003e4668366bdbb6c8cb02b47c
khoajunior <khoajunior666@gmail.com>
2020-03-17 10:27:10 +0700

    catalogy

---
+++ app/src/main/java/com/example/learnengish_android/view/CategoryActivity.java
@@ -0,0 +1,16 @@
+package com.example.learnengish_android.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.learnengish_android.R;
+
+public class CategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category);
+    }
+}
