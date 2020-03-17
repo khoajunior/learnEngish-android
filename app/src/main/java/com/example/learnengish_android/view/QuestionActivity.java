3dc7cdfbd161f7f6854ce9b6ae0d090711a4ae45
khoajunior666 <khoajunior666@gmail.com>
2020-03-17 16:58:06 +0700

    result_activity

--- app/src/main/java/com/example/learnengish_android/view/QuestionActivity.java
+++ app/src/main/java/com/example/learnengish_android/view/QuestionActivity.java
@@ -2,15 +2,32 @@ package com.example.learnengish_android.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.learnengish_android.R;
 
 public class QuestionActivity extends AppCompatActivity {
+    private Button continueButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
+        continueButton = findViewById(R.id.buttonContinue);
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               openResultActivity();
+            }
+        });
     }
+
+    public void openResultActivity(){
+        Intent intent = new Intent(QuestionActivity.this, Result_Activity.class);
+        startActivity(intent);
+    }
+
 }
---
+++ app/src/main/java/com/example/learnengish_android/view/Result_Activity.java
@@ -0,0 +1,16 @@
+package com.example.learnengish_android.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.learnengish_android.R;
+
+public class Result_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_);
+    }
+}
