94d32165e64e674adbc9bd11aff09e695d9f4be1
khoajunior <khoajunior666@gmail.com>
2020-03-19 08:58:21 +0700

    push

--- app/src/main/java/com/example/learnengish_android/MainActivity.java
+++ app/src/main/java/com/example/learnengish_android/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.learnengish_android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.learnengish_android.view.CategoryActivity;
+import com.example.learnengish_android.view.HomeFragmnet;
+import com.example.learnengish_android.view.ProcessFragment;
+import com.example.learnengish_android.view.SettingFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
@@ -23,7 +30,34 @@ public class MainActivity extends AppCompatActivity {
 //                openCategoryActivity();
 //            }
 //        });
+
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragmnet()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selectedFragment = null;
+            switch (menuItem.getItemId()) {
+                case R.id.progress:
+                    selectedFragment = new ProcessFragment();
+                    break;
+                case R.id.menuStart:
+                    selectedFragment = new HomeFragmnet();
+                    break;
+                case R.id.settings:
+                    selectedFragment = new SettingFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+
+            return false;
+        }
+    };
+
 
     public void openCategoryActivity(){
         Intent intent = new Intent(MainActivity.this, CategoryActivity.class);
